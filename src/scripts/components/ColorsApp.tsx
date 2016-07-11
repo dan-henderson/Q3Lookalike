e77b85857983f695e9aecb862d5103e84be0244b
Jamie Humphries <jamie.humphries@softwire.com>
2016-07-11 09:10:27 +0100

    Add "Hello World" scripts.

---
+++ src/scripts/components/ColorsApp.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export class ColorsApp extends React.Component<{}, {}> {
+  public render() {
+    return <h1>Hello world!</h1>;
+  }
+}
---
+++ src/scripts/main.tsx
@@ -0,0 +1,7 @@
+/// <reference path="./main.d.ts" />
+
+import { ColorsApp } from "./components/ColorsApp";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+ReactDOM.render(<ColorsApp />, document.getElementById("content"));
